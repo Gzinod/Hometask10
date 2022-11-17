c625587e9966519e846bb43345a9cb013ddf58bf
GzINOD <gzinod31@rambler.ru>
2022-11-17 20:36:06 +0300

    HomeWork Done

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("num1");
+int num1 = Convert.ToInt32 (Console.ReadLine());
+if (num1 / 100 >=1 && num1 / 100 < 10)
+{
+   num1=num1/100 * 10 + num1 % 10;
+   Console.WriteLine(num1);
+}
+else
+{
+ Console.WriteLine("Incorrect Number");
+}
